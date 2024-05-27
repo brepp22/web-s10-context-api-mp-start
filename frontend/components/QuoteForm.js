0da0f735f7379aaeafbc2b4b097ffd99ddc236d5
brepp22 <brepp@hotmail.com>
2024-05-26 17:11:00 -0700

    project updates

--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -1,7 +1,10 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useContext } from 'react'
+import { QuotesContext } from '../context/quotesContext'
 
 const CHANGE_INPUT = 'CHANGE_INPUT'
 const RESET_FORM = 'RESET_FORM'
+
+
 
 const initialState = {
   authorName: '',
@@ -22,7 +25,7 @@ const reducer = (state, action) => {
 }
 
 export default function TodoForm(props) {
-  const { createQuote } = props
+  const { createQuote } = useContext(QuotesContext)
   const [state, dispatch] = useReducer(reducer, initialState)
   const onChange = ({ target: { name, value } }) => {
     dispatch({ type: CHANGE_INPUT, payload: { name, value } })
